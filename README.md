ef835861539bfe8469b18f699ce6bb5db5011ae5
vfyjxf <2331007009@qq.com>
2022-06-29 21:41:54 +0800

    update environment to 1.18.2

--- README.md
+++
@@ -1,5 +0,0 @@
-# JEI-Utilities
-More enhancements for JEI
-
-## Credits
-Thanks for the idea from NEI easy search
--- src/main/java/com/github/vfyjxf/jeiutilities/JEIUtilities.java
+++ src/main/java/com/github/vfyjxf/jeiutilities/JEIUtilities.java
@@ -1,59 +1,14 @@
 package com.github.vfyjxf.jeiutilities;
 
-import com.github.vfyjxf.jeiutilities.config.JeiUtilitiesConfig;
-import com.github.vfyjxf.jeiutilities.gui.bookmark.BookmarkInputHandler;
-import com.github.vfyjxf.jeiutilities.jei.JeiUtilitiesPlugin;
-import mezz.jei.Internal;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod(modid = JEIUtilities.MODID,
-        name = JEIUtilities.NAME,
-        version = JEIUtilities.VERSION,
-        dependencies = JEIUtilities.DEPENDENCIES,
-        guiFactory = JEIUtilities.GUI_FACTORY,
-        clientSideOnly = true
-)
-public class JEIUtilities {
-    public static final String MODID = "jeiutilities";
-    public static final String NAME = "JEI Utilities";
-    public static final String VERSION = "@VERSION@";
-    public static final String DEPENDENCIES = "required-after:jei";
-    public static final String GUI_FACTORY = "com.github.vfyjxf.jeiutilities.config.JeiUtilitiesConfigGuiFactory";
+@Mod(JeiUtilities.MODE_ID)
+public class JeiUtilities {
 
-    public static final Logger logger = LogManager.getLogger(JEIUtilities.NAME);
+    public static final String MODE_ID = "jeiutilities";
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        JeiUtilitiesConfig.preInit(event);
+    public JeiUtilities(){
+
     }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        logger.info("JEI Utilities Initializing...");
-        if (JeiUtilitiesConfig.getRecordRecipes()) {
-            MinecraftForge.EVENT_BUS.register(BookmarkInputHandler.getInstance());
-        }
-    }
-
-    @EventHandler
-    public void onLoadComplete(FMLLoadCompleteEvent event) {
-        logger.info("JEI Utilities Loading Complete...");
-        if (JeiUtilitiesConfig.getRecordRecipes() || JeiUtilitiesConfig.getEnableHistory()) {
-            JeiUtilitiesPlugin.inputHandler = ObfuscationReflectionHelper.getPrivateValue(Internal.class, null, "inputHandler");
-            if (JeiUtilitiesConfig.getRecordRecipes()) {
-                BookmarkInputHandler.onInputHandlerSet();
-                BookmarkInputHandler.getInstance().getRecipeBookmarkList().loadRecipeInfo();
-            }
-        }
-    }
-
 
 }
