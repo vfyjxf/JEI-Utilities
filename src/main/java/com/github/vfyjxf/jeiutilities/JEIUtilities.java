009d58e9175e6ce6569d80a6be141655b3f86cbe
Tian mi <35869948+MCTian-mi@users.noreply.github.com>
2025-08-28 16:48:35 +0800

    Merge pull request #22 from MCTian-mi/hei-4.29.x
    Fix: compatible with hei 4.29.x

--- src/main/java/com/github/vfyjxf/jeiutilities/JEIUtilities.java
+++ src/main/java/com/github/vfyjxf/jeiutilities/JEIUtilities.java
@@ -1,17 +1,8 @@
 package com.github.vfyjxf.jeiutilities;
 
 import com.github.vfyjxf.jeiutilities.config.JeiUtilitiesConfig;
-import com.github.vfyjxf.jeiutilities.config.KeyBindings;
-import com.github.vfyjxf.jeiutilities.gui.common.GuiInputHandler;
-import com.github.vfyjxf.jeiutilities.gui.bookmark.BookmarkInputHandler;
-import com.github.vfyjxf.jeiutilities.jei.JeiUtilitiesPlugin;
-import mezz.jei.Internal;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -36,27 +27,4 @@ public class JEIUtilities {
     public void preInit(FMLPreInitializationEvent event) {
         JeiUtilitiesConfig.preInit(event);
     }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        logger.info("JEI Utilities Initializing...");
-        if (JeiUtilitiesConfig.getRecordRecipes()) {
-            MinecraftForge.EVENT_BUS.register(BookmarkInputHandler.getInstance());
-        }
-        MinecraftForge.EVENT_BUS.register(GuiInputHandler.getInstance());
-        KeyBindings.registerKeyBindings();
-    }
-
-    @EventHandler
-    public void onLoadComplete(FMLLoadCompleteEvent event) {
-        if (JeiUtilitiesConfig.getRecordRecipes() || JeiUtilitiesConfig.isEnableHistory()) {
-            JeiUtilitiesPlugin.inputHandler = ObfuscationReflectionHelper.getPrivateValue(Internal.class, null, "inputHandler");
-            if (JeiUtilitiesConfig.getRecordRecipes()) {
-                BookmarkInputHandler.getInstance().onInputHandlerSet();
-            }
-        }
-        logger.info("JEI Utilities Loading Complete...");
-    }
-
-
 }
--- src/main/java/com/github/vfyjxf/jeiutilities/jei/JeiUtilitiesPlugin.java
+++ src/main/java/com/github/vfyjxf/jeiutilities/jei/JeiUtilitiesPlugin.java
@@ -1,29 +1,18 @@
 package com.github.vfyjxf.jeiutilities.jei;
 
 import com.github.vfyjxf.jeiutilities.config.JeiUtilitiesConfig;
-import com.github.vfyjxf.jeiutilities.gui.bookmark.AdvancedBookmarkOverlay;
 import com.github.vfyjxf.jeiutilities.gui.history.AdvancedIngredientGrid;
-import com.github.vfyjxf.jeiutilities.jei.ingredient.CraftingRecipeInfo;
-import com.github.vfyjxf.jeiutilities.jei.ingredient.RecipeInfo;
-import com.github.vfyjxf.jeiutilities.jei.ingredient.RecipeInfoHelper;
-import com.github.vfyjxf.jeiutilities.jei.ingredient.RecipeInfoRenderer;
 import mezz.jei.Internal;
 import mezz.jei.api.*;
-import mezz.jei.api.ingredients.IModIngredientRegistration;
-import mezz.jei.bookmarks.BookmarkList;
-import mezz.jei.gui.overlay.IngredientGrid;
 import mezz.jei.gui.overlay.IngredientGridWithNavigation;
 import mezz.jei.gui.overlay.IngredientListOverlay;
-import mezz.jei.gui.overlay.bookmarks.BookmarkOverlay;
 import mezz.jei.ingredients.IngredientListElementFactory;
 import mezz.jei.ingredients.IngredientOrderTracker;
 import mezz.jei.ingredients.IngredientRegistry;
-import mezz.jei.input.InputHandler;
 import mezz.jei.runtime.JeiRuntime;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import javax.annotation.Nonnull;
-import java.util.Collections;
 import java.util.Optional;
 
 import static net.minecraftforge.fml.common.ObfuscationReflectionHelper.getPrivateValue;
@@ -35,20 +24,11 @@ import static net.minecraftforge.fml.common.ObfuscationReflectionHelper.getPriva
 public class JeiUtilitiesPlugin implements IModPlugin {
 
     public static JeiRuntime jeiRuntime;
-    public static InputHandler inputHandler;
     public static IModRegistry modRegistry;
     public static IngredientRegistry ingredientRegistry;
     public static IngredientOrderTracker ORDER_TRACKER;
     public static IGuiHelper guiHelper;
-    public static BookmarkOverlay bookmarkOverlay;
-    public static BookmarkList bookmarkList;
     public static IngredientListOverlay ingredientListOverlay;
-    public static IngredientGrid bookmarkIngredientGrid;
-    public static IngredientGridWithNavigation bookmarkContents;
-    /**
-     * This field is set by asm.
-     */
-    public static IRecipeRegistry recipeRegistry;
     private static AdvancedIngredientGrid grid;
 
     @Override
@@ -64,14 +44,6 @@ public class JeiUtilitiesPlugin implements IModPlugin {
             );
             ORDER_TRACKER = getPrivateValue(IngredientListElementFactory.class, null, "ORDER_TRACKER");
         }
-        if (JeiUtilitiesConfig.getRecordRecipes()) {
-            bookmarkOverlay = (BookmarkOverlay) jeiRuntime.getBookmarkOverlay();
-            bookmarkList = getPrivateValue(BookmarkOverlay.class, bookmarkOverlay, "bookmarkList");
-            bookmarkContents = getPrivateValue(BookmarkOverlay.class, bookmarkOverlay, "contents");
-            bookmarkIngredientGrid = getPrivateValue(IngredientGridWithNavigation.class,
-                    bookmarkContents,
-                    "ingredientGrid");
-        }
     }
 
     @Override
@@ -79,15 +51,6 @@ public class JeiUtilitiesPlugin implements IModPlugin {
         JeiUtilitiesPlugin.modRegistry = registry;
         ingredientRegistry = (IngredientRegistry) registry.getIngredientRegistry();
         JeiUtilitiesPlugin.guiHelper = registry.getJeiHelpers().getGuiHelper();
-    }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    @Override
-    public void registerIngredients(@Nonnull IModIngredientRegistration registry) {
-        RecipeInfoHelper helper = new RecipeInfoHelper<>();
-        RecipeInfoRenderer renderer = new RecipeInfoRenderer();
-        registry.register(RecipeInfo.RECIPE_INFO, Collections.emptyList(), helper, renderer);
-        registry.register(CraftingRecipeInfo.CRAFTING_RECIPE_INFO, Collections.emptyList(), helper, renderer);
     }
 
     public static Optional<AdvancedIngredientGrid> getGrid() {
